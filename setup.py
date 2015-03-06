6989c27aae6e7bcfd10cf9f599a6793e1afef906
Kevin McCarthy <me@kevinmccarthy.org>
2015-03-06 10:01:06 -1000

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python
 
-import sys
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-
-class PyTest(TestCommand):
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
 
 setup(name='rebnypy',
       version='0.0.1',
@@ -31,6 +15,7 @@ setup(name='rebnypy',
         'rebnypy': 'rebnypy',
       },
       license='MIT',
+      install_requires=['requests'],
       tests_require=['pytest','mock'],
       cmdclass={'test': PyTest},
       classifiers=[
