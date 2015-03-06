de475264e39ea23a5560a7657236471c553047fd
Kevin McCarthy <me@kevinmccarthy.org>
2015-03-06 10:53:57 -1000

    Remove all None values

--- rebnypy/client.py
+++ rebnypy/client.py
@@ -11,6 +11,13 @@ def get_date_string(date):
     """
     return date.strftime("%m/%d/%Y %H:%M %p")
 
+def strip_nones(row):
+    """
+    Remove all items with None for a value, because why include it if there's
+    no value?
+    """
+    return dict([(k,v) for k, v in row.items() if v is not None])
+
 class RebnyClient(object):
 
     def __init__(self, endpoint, api_key):
@@ -24,7 +31,7 @@ class RebnyClient(object):
 
     def _get(self, url):
         rows = self._request(url)
-        return [lookups.expand_row(r) for r in rows]
+        return [strip_nones(lookups.expand_row(r)) for r in rows]
 
     def get_all_listings(self):
         url = self.endpoint + '/listings'
